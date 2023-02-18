d7a987236707db65bb4d3323feb1ab3f873805fb
Sebastian Misiewicz <sebastian@sebastianmisiewicz.info>
2023-02-18 10:34:36 +0100

    Initial commit

---
+++ src/main/java/com/progsapps/springkeycloakintegration/SpringKeycloakIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.progsapps.springkeycloakintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKeycloakIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKeycloakIntegrationApplication.class, args);
+    }
+
+}
