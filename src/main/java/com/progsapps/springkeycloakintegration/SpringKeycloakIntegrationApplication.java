b7124c0784d108dd6937c900c95b98837818e026
Sebastian Misiewicz <sebastian@sebastianmisiewicz.info>
2023-02-20 07:52:07 +0100

    Use oauth2 libraries

--- src/main/java/com/progsapps/springkeycloakintegration/SpringKeycloakIntegrationApplication.java
+++ src/main/java/com/progsapps/springkeycloakintegration/SpringKeycloakIntegrationApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringKeycloakIntegrationApplication {
@@ -14,8 +15,8 @@ public class SpringKeycloakIntegrationApplication {
     }
 
     @Bean
-    public ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
-        return new ServletListenerRegistrationBean<HttpSessionEventPublisher>(new HttpSessionEventPublisher());
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
--- src/main/java/com/progsapps/springkeycloakintegration/controller/SecurityController.java
+++ src/main/java/com/progsapps/springkeycloakintegration/controller/SecurityController.java
@@ -1,15 +1,15 @@
 package com.progsapps.springkeycloakintegration.controller;
 
 import com.progsapps.springkeycloakintegration.model.User;
-import org.keycloak.KeycloakPrincipal;
-import org.keycloak.KeycloakSecurityContext;
-import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
 import java.util.logging.Logger;
 
 @RestController
@@ -18,18 +18,21 @@ public class SecurityController {
 
     public static Logger LOGGER = Logger.getLogger(ProductController.class.getName());
 
+    private final OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager;
+
+    private final OAuth2AuthorizedClient oAuth2AuthorizedClient;
+
+    public SecurityController(OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager, OAuth2AuthorizedClient oAuth2AuthorizedClient) {
+        this.oAuth2AuthorizedClientManager = oAuth2AuthorizedClientManager;
+        this.oAuth2AuthorizedClient = oAuth2AuthorizedClient;
+    }
+
+
     @GetMapping(produces = "application/json", path = "current")
-    public User getUser() {
-        LOGGER.info(String.valueOf(SecurityContextHolder.getContext().getAuthentication().getPrincipal()));
-        KeycloakAuthenticationToken authentication =
-                (KeycloakAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
-
-        Principal principal = (Principal) authentication.getPrincipal();
-
-        KeycloakPrincipal<KeycloakSecurityContext> kPrincipal = (KeycloakPrincipal<KeycloakSecurityContext>) principal;
-
-        return new User(kPrincipal.getKeycloakSecurityContext().getIdToken().getPreferredUsername(),
-                kPrincipal.getKeycloakSecurityContext().getToken().getRealmAccess().getRoles());
+    public User getUser(Authentication auth) {
+        OidcUser principal = (OidcUser) auth.getPrincipal();
+        oAuth2AuthorizedClientManager.authorize(OAuth2AuthorizeRequest.withAuthorizedClient(oAuth2AuthorizedClient).build());
+        return null;
     }
 
 }
