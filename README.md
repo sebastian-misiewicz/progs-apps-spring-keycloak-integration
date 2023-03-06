296439fd9e665bef874d915f72b0f6b3a1ce1d14
Sebastian Misiewicz <sebastian@sebastianmisiewicz.info>
2023-03-06 11:20:06 +0100

    Update documentation

--- README.md
+++ README.md
@@ -34,5 +34,66 @@ docker-compose up -d
 4. Get data using access token
 ![OAuth2 communication](.assets/documentation-oauth2-communication.png)
 
-## 
+## Summary
+**A /** Use `spring-boot-starter-security` and `spring-boot-starter-oauth2-resource-server` libraries to integrate Keycloak. Add dependencies in `pom.xml`:
+```xml
+<dependencies>
+  <dependency>
+      <groupId>org.springframework.boot</groupId>
+      <artifactId>spring-boot-starter-web</artifactId>
+      <version>${spring.boot.version}</version>
+  </dependency>
+  <dependency>
+      <groupId>org.springframework.boot</groupId>
+      <artifactId>spring-boot-starter-security</artifactId>
+      <version>${spring.boot.version}</version>
+  </dependency>
+  <dependency>
+      <groupId>org.springframework.boot</groupId>
+      <artifactId>spring-boot-starter-oauth2-resource-server</artifactId>
+      <version>${spring.boot.version}</version>
+  </dependency>
+</dependencies>
+```
 
+**B /** Add new properties to `application.properties`:
+1. Keycloak client ID:
+```
+spring.security.oauth2.client.registration.keycloak.client-id=progs-apps-client
+```
+2. Scope:
+```
+spring.security.oauth2.client.registration.keycloak.scope=openid
+```
+3. Issuer uri:
+```
+spring.security.oauth2.resourceserver.jwt.issuer-uri=http://keycloak:8080/realms/ProgsApps
+```
+
+
+**C /** Configuration of security `com.progsapps.springkeycloakintegration.WebSecurityConfig.filterChain`:
+```java
+@Bean
+public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+  http.cors().and()
+     .authorizeRequests()
+     .antMatchers("/product*", "/security/current*")
+     .hasRole("progs-apps-regular-user")
+     .anyRequest()
+     .permitAll();
+  http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+  return http.csrf().disable().build();
+}
+```
+> **Note**: Spring requires roles to be prefixed with `ROLE_`. Here we configure role without a prefix. 
+
+**D /**
+To map roles from JWT Token to spring security roles `JwtRolesConverter` has been implemented and integrated with `JwtAuthenticationConverter`:
+```java
+@Bean
+public JwtAuthenticationConverter jwtAuthenticationConverter(JwtRolesConverter jwtRolesConverter) {
+  JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+  jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtRolesConverter);
+  return jwtAuthenticationConverter;
+}
+```
--- src/main/java/com/progsapps/springkeycloakintegration/WebSecurityConfig.java
+++ src/main/java/com/progsapps/springkeycloakintegration/WebSecurityConfig.java
@@ -2,28 +2,15 @@ package com.progsapps.springkeycloakintegration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.session.SessionRegistryImpl;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-
-    private final KeycloakLogoutHandler keycloakLogoutHandler;
-
-    public WebSecurityConfig(KeycloakLogoutHandler keycloakLogoutHandler) {
-        this.keycloakLogoutHandler = keycloakLogoutHandler;
-    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -33,19 +20,8 @@ public class WebSecurityConfig {
                 .hasRole("progs-apps-regular-user")
                 .anyRequest()
                 .permitAll();
-        http.oauth2Login()
-                .and()
-                .logout()
-                .addLogoutHandler(keycloakLogoutHandler)
-                .logoutSuccessUrl("/");
         http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
         return http.csrf().disable().build();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-        return http.getSharedObject(AuthenticationManagerBuilder.class)
-                .build();
     }
 
     @Bean
